4314691a2c3471174f9eff159c0e048065cdcddd
Kazerlelutin <b@bouteiller.contact>
2022-11-26 18:50:10 +0100

    fix(twtich): redirect

--- src/routes/[...lang]/twitch/index@noauth.tsx
+++ src/routes/[...lang]/twitch/index@noauth.tsx
@@ -1,4 +1,4 @@
-import { component$, useClientEffect$ } from '@builder.io/qwik'
+import { component$, useClientEffect$, $ } from '@builder.io/qwik'
 import { DocumentHead } from '@builder.io/qwik-city'
 import queryString from 'query-string'
 import { setCookie } from '~/utils/set-cookie'
@@ -8,16 +8,18 @@ export default component$(() => {
   //const nav = useNavigate()
 
   useClientEffect$(() => {
-    const tokens: null | { access_token?: string } = queryString.parse(
-      document.location.hash.replace('#', '')
-    )
-    if (tokens && tokens?.access_token) {
-      setCookie(tokens.access_token)
+    $(() => {
+      const tokens: null | { access_token?: string } = queryString.parse(
+        document.location.hash.replace('#', '')
+      )
+      if (tokens && tokens?.access_token) {
+        setCookie(tokens.access_token)
 
-      //TODO use nav.path when qwik fix context error
-      window.location.pathname = '/dashboard'
-      // nav.path = '/dashboard'
-    }
+        //TODO use nav.path when qwik fix context error
+        window.location.pathname = '/dashboard'
+        // nav.path = '/dashboard'
+      }
+    })()
   })
 
   return (
