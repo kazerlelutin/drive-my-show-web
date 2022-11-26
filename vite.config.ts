ab82eb66ff367c682dc1ba10bcfbb0be34fc8f3a
Kazerlelutin <b@bouteiller.contact>
2022-11-26 18:59:37 +0100

    fix(translate): add to vite confi

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,26 @@
-import { defineConfig } from 'vite';
-import { qwikVite } from '@builder.io/qwik/optimizer';
-import { qwikCity } from '@builder.io/qwik-city/vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vite'
+import { qwikVite } from '@builder.io/qwik/optimizer'
+import { qwikCity } from '@builder.io/qwik-city/vite'
+import { qwikSpeakInline } from 'qwik-speak/inline'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig(() => {
   return {
-    plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
+    plugins: [
+      qwikCity(),
+      qwikVite(),
+      tsconfigPaths(),
+      qwikSpeakInline({
+        basePath: './',
+        assetsPath: 'public/i18n',
+        supportedLangs: ['en-US', 'it-IT'],
+        defaultLang: 'en-US',
+      }),
+    ],
     preview: {
       headers: {
         'Cache-Control': 'public, max-age=600',
       },
     },
-  };
-});
+  }
+})
